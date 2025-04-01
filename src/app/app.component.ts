4917656e8af7d4c3708f6fbcee8fe806e6400b52
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-01 18:56:40 -0300

    adcionado componente mat-slide-toggle para teste de front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
