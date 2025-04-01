310d17b46d3c6a6d48ec3cd760e7019496759e8b
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-01 19:05:05 -0300

    Criado componente de consulta

---
+++ src/app/components/consulta/consulta.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta',
+  imports: [],
+  template: `<p>consulta works!</p>`,
+  styleUrl: './consulta.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConsultaComponent { }
