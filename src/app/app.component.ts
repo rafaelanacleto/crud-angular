cb9ffaf87a8657fdec1c79da92a8069edee5595b
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-06 08:51:55 -0300

    Criando menu superior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ConsultaComponent } from "./components/consulta/consulta.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, ConsultaComponent, ConsultaComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/components/consulta/consulta.component.ts
+++ src/app/components/consulta/consulta.component.ts
@@ -1,9 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
-  template: `<p>consulta works!</p>`,
+  imports: [MatToolbarModule],
+  template: `
+  
+  <mat-toolbar color="primary">
+    <span>Consulta</span>
+    <span class="spacer"></span>
+    <button mat-button routerLink="/consulta/novo">Novo</button>
+  </mat-toolbar>`,
   styleUrl: './consulta.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
