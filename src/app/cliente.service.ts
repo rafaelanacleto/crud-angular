de41bd95097c8c3b68b2ba53ee649f4109198ab2
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-14 13:26:32 -0300

    Adiciona o serviço Cliente e implementa a lógica de salvamento; atualiza o componente Cadastro com novos campos e ações; define cor de fundo global.

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Cliente } from './components/cadastro/cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,9 @@ import { Injectable } from '@angular/core';
 export class ClienteService {
 
   constructor() { }
+
+  salvar(cliente: Cliente): void {
+    // Aqui você pode implementar a lógica para salvar o cliente, como enviar os dados para um servidor ou armazená-los localmente.
+    console.log('Cliente salvo:', cliente); 
+  }
 }
--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -8,14 +8,27 @@ import { MatIcon } from '@angular/material/icon';
 import { MatSelect } from '@angular/material/select';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatLabel } from '@angular/material/form-field';
 import { MatError } from '@angular/material/form-field';
 import { Cliente } from './cliente';
+import { ClienteService } from '../../cliente.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule, MatFormField, MatInput, MatButton, MatIcon, MatSelect, ReactiveFormsModule, MatLabel, MatError],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatIcon,
+    MatSelect,
+    ReactiveFormsModule,
+    MatLabel,
+    MatError,
+  ],
   templateUrl: './cadastro.component.html',
   standalone: true,
   styleUrl: './cadastro.component.css',
@@ -25,7 +38,9 @@ export class CadastroComponent {
   ngForm: any;
   cliente: Cliente = new Cliente('', '', '', '');
 
-  onSubmit(): void {
-    console.log('Form submitted:', this.ngForm.value);
+  constructor(private clienteService: ClienteService) {}
+
+  salvar() {
+    this.clienteService.salvar(this.cliente);
   }
 }
