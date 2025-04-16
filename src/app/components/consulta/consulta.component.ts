d70df5ea4af436a8f3c42942e016c2989fe879e7
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-16 16:03:58 -0300

    Atualiza dependência do Angular para a versão 19.2.6; adiciona estrutura e estilos ao componente Consulta.

--- src/app/components/consulta/consulta.component.ts
+++ src/app/components/consulta/consulta.component.ts
@@ -1,10 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
-  template: `<p>consulta works!</p>`,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatCardModule, FlexLayoutModule, MatIconModule],
+  standalone: true,
+  templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConsultaComponent { }
+export class ConsultaComponent {}
