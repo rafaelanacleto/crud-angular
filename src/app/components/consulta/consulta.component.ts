b2d99370901013fc8580d3f4d979a53a7a0cef29
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-17 19:30:46 -0300

    Refatora o componente Consulta para incluir campos de pesquisa adicionais e lógica de exibição de resultados; ajusta layout e estilos.

--- src/app/components/consulta/consulta.component.ts
+++ src/app/components/consulta/consulta.component.ts
@@ -4,12 +4,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Cliente } from '../cadastro/cliente';
+import {MatTableModule} from '@angular/material/table';
+import { ClienteService } from '../../cliente.service';
 
 @Component({
   selector: 'app-consulta',
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatCardModule, FlexLayoutModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatCardModule, FlexLayoutModule, MatIconModule, MatButton, MatTableModule],
   standalone: true,
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.css',
@@ -18,9 +21,13 @@ import { Cliente } from '../cadastro/cliente';
 export class ConsultaComponent {
 
   filtro : Cliente = new Cliente('', '', '', '');
+  clientes: Cliente[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'email', 'telefone', 'acoes'];
+
+  constructor(private clienteService: ClienteService) {}
 
   pesquisar() {
-    // Implementar lógica de pesquisa
+    this.clientes = this.clienteService.obterStorage();
   }
   limpar() {
     this.filtro = new Cliente('', '', '', '');
