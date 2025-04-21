d879740f11527cc312298e51b8ad9b01d07aa159
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-21 18:24:52 -0300

    Adiciona campo de telefone ao formulário de cadastro e implementa lógica de filtro na tabela de consulta, melhoria no CSS do componente consulta.component

--- src/app/components/consulta/consulta.component.ts
+++ src/app/components/consulta/consulta.component.ts
@@ -7,7 +7,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Cliente } from '../cadastro/cliente';
-import {MatTableModule} from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { ClienteService } from '../../cliente.service';
 import { CommonModule } from '@angular/common';
 
@@ -23,25 +23,18 @@ export class ConsultaComponent {
 
   filtro : Cliente = new Cliente('', '', '', '');
   clientes: Cliente[] = [];
-  displayedColumns: string[] = ['id', 'nome', 'email', 'telefone', 'acoes'];
+  displayedColumns: string[] = ['id', 'nome', 'email', 'telefone'];
+  dataSource = new MatTableDataSource<Cliente>(this.clientes);
 
-  constructor(private clienteService: ClienteService) {}
+  constructor(private clienteService: ClienteService) {
 
-  pesquisar() {
     this.clientes = this.clienteService.obterStorage();
+    this.dataSource = new MatTableDataSource(this.clientes);
   }
-  limpar() {
-    this.filtro = new Cliente('', '', '', '');
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-  // Implementar lógica de limpeza
-  // Implementar lógica de exibição de resultados
-  // Implementar lógica de exibição de detalhes do cliente
-  // Implementar lógica de edição do cliente
-  // Implementar lógica de exclusão do cliente
-  // Implementar lógica de exibição de mensagem de sucesso
-  // Implementar lógica de exibição de mensagem de erro
-  // Implementar lógica de exibição de mensagem de confirmação
-  // Implementar lógica de exibição de mensagem de aviso
-  // Implementar lógica de exibição de mensagem de informação
 
 }
